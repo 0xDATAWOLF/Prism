232dd10cd8638572215f2df30efcdb05f8331490
0xDATAWOLF <magiktrikdev@gmail.com>
2020-06-28 12:25:42 -0500

    Streamlined Layer & Scene

--- Prism/Prism/src/Prism.h
+++ Prism/Prism/src/Prism.h
@@ -1,11 +1,11 @@
 #pragma once
 
 // --- Core Files --------------------------
-#include "core/core.h"
-#include "core/logger.h"
-#include "core/events.h"
-#include "core/application.h"
-#include "core/window.h"
-#include "core/scene.h"
-#include "core/layer.h"
-#include "core/entry.h"
+#include <core/core.h>
+#include <core/logger.h>
+#include <core/application.h>
+#include <core/scene.h>
+#include <core/layer.h>
+
+// --- Entry -------------------------------
+#include <core/entry.h>
--- Prism/Sandbox/src/sandbox.cpp
+++ Prism/Sandbox/src/sandbox.cpp
@@ -1,9 +1,49 @@
 #include <Prism.h>
 
+class ExampleLayer : public Prism::Layer {
+
+public:
+	inline ExampleLayer() {
+		PRISM_INFO("ExampleLayer constructed.");
+	};
+	inline virtual ~ExampleLayer() {
+		PRISM_INFO("ExampleLayer removed.");
+	};
+
+	void Update() override {
+		PRISM_INFO("ExampleLayer updated.");
+	}
+
+	
+};
+
+class ExampleScene : public Prism::Scene {
+
+public:
+	inline ExampleScene() {};
+	inline virtual ~ExampleScene() {};
+
+	inline void Update() override {
+		updateCount++;
+		if (updateCount >= 5) {
+			RemoveLayer(exLayer);
+		}
+	};
+
+	uint16_t updateCount = 0;
+	Prism::Layer* exLayer;
+
+};
+
 class SandboxApplication : public Prism::Application {
 public:
 
-	SandboxApplication() {};
+	SandboxApplication() : Application(1280, 720, "Prism Engine - Sandbox") {
+		SetScene<ExampleScene>();
+		auto currentScene = GetScene<ExampleScene>();
+		Prism::Layer* pLayer = currentScene->PushLayer<ExampleLayer>();
+		currentScene->exLayer = pLayer;
+	};
 	~SandboxApplication() {};
 
 };
